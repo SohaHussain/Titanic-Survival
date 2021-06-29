420fc4e3d698c89e94cfcec79ceeee1dbf82277b
Soha <soha00hussain@gmail.com>
2021-06-29 23:23:06 +0530

    libraries imported

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print("testing")
+## Titanic Survival
+
+#IMPORTING LIBRARIES
+
+# linear algebra
+import numpy as np
+
+# data processing
+import pandas as pd
+
+#data visualization
+import seaborn as sns
+from matplotlib import pyplot as plt
+from matplotlib import style
+
+#algorithms
+from sklearn import linear_model
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC, LinearSVC
+from sklearn.naive_bayes import GaussianNB
+
+#getting data
+test_df = pd.read_csv("test.csv")
+train_df = pd.read_csv("train.csv")
+
+
+
+
+
