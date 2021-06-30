6640780aaaf66b9439190b16c5a52480e784213e
Soha <soha00hussain@gmail.com>
2021-06-30 12:10:29 +0530

    data analysis

--- main.py
+++ main.py
@@ -47,6 +47,29 @@ train_df.describe()
 
 train_df.head()
 
+# we need to convert a lot of features into numeric ones later on, so that the machine learning algorithms can process
+# them. Furthermore, we can see that the features have widely different ranges, that we will need to convert into
+# roughly the same scale. We can also spot some more features, that contain missing values (NaN = not a number),
+# that we need to deal with.
+
+#let's take a more detailed look at what data are missing
+
+total = train_df.isnull().sum().sort_values(ascending=False)
+percent1 = train_df.isnull().sum()/train_df.isnull().count() * 100
+percent = (round(percent1, 1)).sort_values(ascending=False)
+missing_data = pd.concat([total, percent], axis=1, keys=['Total', '%'])
+missing_data.head()
+
+#The Embarked feature has only 2 missing values, which can easily be filled. It will be much more tricky, to deal with
+# #the ‘Age’ feature, which has 177 missing values. The ‘Cabin’ feature needs further investigation, but it looks like
+# #that we might want to drop it from the dataset, since 77 % of it are missing.
+
+train_df.columns.values
+
+
+
+
+
 
 
 
